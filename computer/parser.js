ec73449b645986300c97426d8b1a68956979714d
TenFont <83959297+TenFont@users.noreply.github.com>
2022-09-11 12:44:08 +0500

    Update parser.js

--- computer/parser.js
+++ computer/parser.js
@@ -1,19 +1,20 @@
 # Parse string into program
 function parse(code: string) :: nbtcompound:
-  set {_state} to {_code}
-  return program({_state})
+  set {_state} to nbt compound from "{}"
+  set string tag "string" of {_state} to {_code}
+  return Program({_state})
 
 # Program
-#   - numberLiteral
-function Program(state: string) :: nbtcompound:
-  return numberLiteral({_state})
+#   - NumberLiteral
+function Program(state: nbtcompound) :: nbtcompound:
+  set {_compound} to nbt compound from "{type:""program""}"
+  set compound tag "body" of {_compound} to NumberLiteral({_state})
+  return {_compound}
 
 # NumberLiteral
 #   - NUMBER
-function NumberLiteral(state: string) :: nbtcompound:
-  set {_compound} to nbt compound from "{Type:""Literal""}"
-  set double tag "Value" of {_compound} to {_state} parsed as number
+function NumberLiteral(state: nbtcompound) :: nbtcompound:
+  set {_compound} to nbt compound from "{type:""NumberLiteral""}"
+  set {_string} to string tag "string" of {_state}
+  set double tag "value" of {_compound} to {_string} parsed as number
   return {_compound}
-
-on load:
-  broadcast parse("3")
